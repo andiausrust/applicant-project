aa3e2fbb3ca6297b2d77c9e4bb0f020721b1c8a2
andiausrust <42858913+andiausrust@users.noreply.github.com>
2021-09-29 12:58:55 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,5 @@ export const environment = {
   production: true,
   appVersion: require('../../package.json').version,
   firebaseConfig: {
-    apiKey: "AIzaSyB-ElDvYZ7BmGqEdJ6bHXO9_UScLKgWJbM",
-    authDomain: "fir-12dc8.firebaseapp.com",
-    databaseURL: "https://fir-12dc8.firebaseio.com",
-    projectId: "fir-12dc8",
-    storageBucket: "fir-12dc8.appspot.com",
-    messagingSenderId: "687791679806",
-    appId: "1:687791679806:web:a8392f28041962fff3246f",
-    measurementId: "G-20LQKBLWBK"
   }
 };
